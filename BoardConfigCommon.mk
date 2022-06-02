065a4b6c40ee0ecb9fcedd47d684fef358490a6f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-02 14:06:35 +0300

    freccia-common: Add arch and bootloader flags
    Change-Id: I578f01a20d604fd8c936cffbeed83b967906f638

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,24 @@
 
 COMMON_PATH := device/trenitalia/freccia-common
 
+# Architecture
+TARGET_ARCH := engine
+TARGET_ARCH_VARIANT := electric-engine
+TARGET_CPU_ABI := electric-engine
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic-engine
+TARGET_CPU_VARIANT_RUNTIME := choo-choo
+
+TARGET_2ND_ARCH := engine
+TARGET_2ND_ARCH_VARIANT := carbon-engine
+TARGET_2ND_CPU_ABI := carbon-engine
+TARGET_2ND_CPU_ABI2 := carbon-eabi
+TARGET_2ND_CPU_VARIANT := generic-engine
+TARGET_2ND_CPU_VARIANT_RUNTIME := choo-choo
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := kona
+TARGET_NO_BOOTLOADER := true
+
 # Inherit the proprietary files
 include vendor/trenitalia/freccia-common/BoardConfigVendor.mk
